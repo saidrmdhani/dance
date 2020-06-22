8d99b615ca1202ff8b6239680e9e7efd14abb10a
Said Al Ramadhani <said.alramadhani@bss.om>
2020-06-22 14:22:50 +0400

    Add Google analytics

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -19,6 +19,16 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}"></script>
     @livewireStyles
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-170317783-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-170317783-1');
+    </script>
 </head>
 <body>
     <!-- Header -->
